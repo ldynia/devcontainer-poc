df8039c9e1a49eb173bcbc390e233c4ac2380ea3
ldynia <l_dynowski@yahoo.com>
2023-09-07 13:02:39 +0200

    Update init.sh

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 function install() {
-  curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-    && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+  curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc > /dev/null
+  echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
 
-  npm install -g wscat  
+  npm install -g wscat
 
   sudo apt update && sudo apt install -y \
       bash-completion \
       git-core \
-      ngrok 
+      ngrok
 }
 
 function configure() {
