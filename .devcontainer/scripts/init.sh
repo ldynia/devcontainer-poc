ad5d4122344e81a41c983e0952258418352ae784
ldynia <l_dynowski@yahoo.com>
2023-09-07 12:30:54 +0200

    Update devcontainer.json

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-function provisiton() {
-  npm install -g wscat
+function install() {
   curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-    && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list \
-    && sudo apt update \
-    && sudo apt install ngrok
+    && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+
+  npm install -g wscat  
+
+  sudo apt update && sudo apt install -y \
+      bash-completion \
+      git-core \
+      ngrok 
+}
+
+function configure() {
+  source /usr/share/bash-completion/completions/git
+  source /usr/share/bash-completion/completions/docker
 }
 
 # Execut function(s)
-provisiton
+install
+configure
